c91a8c771e9a0d7890284fc7e41aaa94871b7081
asdacosta <asdacosta18@gmail.com>
2023-10-31 06:38:24 +0000

    Click on buttons should only have effect for Duo selection and restart button should clear contents

--- script.js
+++ script.js
@@ -6,32 +6,44 @@ const score1 = document.querySelector('.score1');
 const score2 = document.querySelector('.score2');
 
 restartButton.addEventListener('click', () => {
-    duo.takeNames();
+    if (select.options[select.selectedIndex].value === 'friend') {
+        duo.takeNames();
+    }
+    emptyButtons();
 })
+
+function emptyButtons () {
+    gameButtons.forEach((button) => {
+        button.textContent = '';
+    })
+}
 
 const duo = (function () {
     function takeNames () {
         const player1 = document.querySelector('.player1');
         const player2 = document.querySelector('.player2');
 
-        if (select.options[select.selectedIndex].value === 'friend') {
             player1.textContent = prompt('Enter first player name:');
             player2.textContent = prompt('Enter second player name:');
-        }
+    }
+
+    function choosePick () {
+        let currentPick = '✯';
+        gameButtons.forEach((button) => {
+            button.addEventListener('click', () => {
+                if (!button.textContent) {
+                    button.textContent = currentPick;
+                    currentPick = currentPick === '✯' ? '🌔' : '✯';
+                }
+            })
+        })
     }
 
     select.addEventListener('change', () => {
-        takeNames();
-    })
-
-    let currentPick = 'X';
-    gameButtons.forEach((button) => {
-        button.addEventListener('click', () => {
-            if (!button.textContent) {
-                button.textContent = currentPick;
-                currentPick = currentPick === 'X' ? 'O' : 'X';
-            }
-        })
+        if (select.options[select.selectedIndex].value === 'friend') {
+            takeNames();
+            choosePick();
+        }
     })
 
     return {takeNames};
