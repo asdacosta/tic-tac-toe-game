521213efb0def29fb3b0357256cebf7bdbe72130
asdacosta <asdacosta18@gmail.com>
2023-10-31 14:11:19 +0000

    Set conditions when there's match or not and give feedback accordingly

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const select = document.querySelector('select');
 const restartButton = document.querySelector('section + button');
 const gameButtons = document.querySelectorAll('.real-box button');
+const feedback = document.querySelector('.feedback');
 
 
 const users = (function () {
     const user1 = document.querySelector('.player1');
     const user2 = document.querySelector('.player2');
 
-    return {user1, user2};
+    let score1 = 0;
+    let score2 = 0;
+
+    return {user1, user2, score1, score2};
 })()
 
 restartButton.addEventListener('click', () => {
@@ -20,6 +24,8 @@ restartButton.addEventListener('click', () => {
 function emptyButtons () {
     gameButtons.forEach((button) => {
         button.textContent = '';
+        button.style.backgroundColor = '';
+        feedback.textContent = 'Begin Game';
     })
 }
 
@@ -54,9 +60,30 @@ const duo = (function () {
 
 
 const checkMatch = function () {
+    const firstUserScore = document.querySelector('.score1');
+    const secUserScore = document.querySelector('.score2');
 
-    const score1 = document.querySelector('.score1');
-    const score2 = document.querySelector('.score2');
+    function update () {
+        if (gameButtons[m].textContent === 'X') {
+            users.score1 += 1;
+            firstUserScore.textContent = `${users.score1}`;
+
+            if (users.user1.textContent === 'User' && users.user2.textContent === 'AI') {
+                feedback.textContent = 'You conquered this round!';
+            } else {
+                feedback.textContent = `${users.user1.textContent} conquered this round!`;
+            }
+        } else if (gameButtons[m].textContent === '🌔') {
+            users.score2 += 1;
+            secUserScore.textContent = `${users.score2}`;
+
+            if (users.user1.textContent === 'User' && users.user2.textContent === 'AI') {
+                feedback.textContent = 'You lost this round.';
+            } else {
+                feedback.textContent = `${users.user2.textContent} conquered this round!`;
+            }
+        }
+    }
 
     for (m = 0; m < 7; m += 3) {
         // Horizontal buttons
@@ -64,16 +91,31 @@ const checkMatch = function () {
             gameButtons[m].style.backgroundColor = 'rgba(172, 248, 86, 0.5)';
             gameButtons[m+1].style.backgroundColor= 'rgba(172, 248, 86, 0.5)';
             gameButtons[m+2].style.backgroundColor = 'rgba(172, 248, 86, 0.5)';
+
+            update();
+            setTimeout(emptyButtons, 2000);
         // Vertical buttons
         } else if ((gameButtons[m/3].textContent === '🌔' || gameButtons[m/3].textContent === 'X') && gameButtons[m/3].textContent === gameButtons[m/3+3].textContent && gameButtons[m/3+3].textContent === gameButtons[m/3+6].textContent) {
             gameButtons[m/3].style.backgroundColor = 'rgba(172, 248, 86, 0.5)';
             gameButtons[m/3+3].style.backgroundColor= 'rgba(172, 248, 86, 0.5)';
             gameButtons[m/3+6].style.backgroundColor = 'rgba(172, 248, 86, 0.5)';
+
+            update();
+            setTimeout(emptyButtons, 2000);
         // Diagonal buttons
-        } else if ((gameButtons[m/1.5].textContent === '🌔' || gameButtons[m/1.5].textContent === 'X') && gameButtons[m/1.5].textContent === gameButtons[(4*m)/3].textContent && gameButtons[(4*m)/3].textContent === gameButtons[-m/1.5+8].textContent) {
-            gameButtons[m/1.5].style.backgroundColor = 'rgba(172, 248, 86, 0.5)';
-            gameButtons[(4*m)/3].style.backgroundColor= 'rgba(172, 248, 86, 0.5)';
-            gameButtons[-m/1.5+8].style.backgroundColor = 'rgba(172, 248, 86, 0.5)';
-        }
+        } else if ((gameButtons[m/1.5].textContent === '🌔' || gameButtons[m/1.5].textContent === 'X') && gameButtons[m/1.5].textContent === gameButtons[4].textContent && gameButtons[4].textContent === gameButtons[-m/1.5+8].textContent) {
+            if (m !== 6) {
+                gameButtons[m/1.5].style.backgroundColor = 'rgba(172, 248, 86, 0.5)';
+                gameButtons[4].style.backgroundColor= 'rgba(172, 248, 86, 0.5)';
+                gameButtons[-m/1.5+8].style.backgroundColor = 'rgba(172, 248, 86, 0.5)';
+    
+                update();
+                setTimeout(emptyButtons, 2000);
+            }
+        } 
+    }
+    if (Array.from(gameButtons).every(button => button.textContent !== '')) {
+        feedback.textContent = "It's a tie :|";
+        setTimeout(emptyButtons, 2000);
     }
 }
